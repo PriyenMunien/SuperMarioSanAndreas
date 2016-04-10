85d2f1e82f59e81a8dd36d668078e273cb981009
Kahilt <kahilt11@gmail.com>
2016-04-10 11:27:46 +0200

    SuperMario header edited along with constructor

--- SuperMarioSanAndreas/SuperMario.cpp
+++ SuperMarioSanAndreas/SuperMario.cpp
@@ -3,6 +3,16 @@
 
 SuperMario::SuperMario()
 {
+	sourceX = 0;
+	sourceY = 0;
+	done = false;
+	active = false;
+	draw = true;
+	x = 10;
+	y = 10;//starting co-ordinates on (x;y) is (10;10)
+	moveSpeed = 5;
+	check = 0;
+	dir = DOWN;//the initial direction of Mario is set to down
 }
 
 
--- SuperMarioSanAndreas/SuperMario.h
+++ SuperMarioSanAndreas/SuperMario.h
@@ -9,17 +9,17 @@ class SuperMario
 public:
 	SuperMario();//default constructor
 	~SuperMario();//default destructor
-	int sourceX = 0, sourceY = 0;//used for cropping the SpriteSheet for animation
+	int sourceX , sourceY ;//used for cropping the SpriteSheet for animation
 
 	enum DIRECTION{ RIGHT, LEFT, DOWN, UP, NONE1, NONE2 }; //Defines the different states or directions of mario. NONE1=facing right NONE2=facing left	
 
 
-	bool done = false;//used to determine when game will exit
-	bool active = false; //will help cause the animation ONLY if key is pressed in particular direction
-	bool draw = true;//for timer, used for smooth animations
-	float x = 10, y = 10, moveSpeed = 5; // x and y are starting co-ordinates of Mario, moveSpeed is the increment of the co-ordinates hence will det how fast Mario moves
+	bool done ;//used to determine when game will exit
+	bool active; //will help cause the animation ONLY if key is pressed in particular direction
+	bool draw ;//for timer, used for smooth animations
+	float x , y , moveSpeed ; // x and y are starting co-ordinates of Mario, moveSpeed is the increment of the co-ordinates hence will det how fast Mario moves
 
-	const float FPS = 100; //used for the timer, will help determine the number of times the game will refresh per second (frames/sec)
-	int check = 0; //will record Marios last left or right movement to decide which side he will face after the key is left
-	int dir = DOWN; //the initial direction of Mario is set to down
+	const float FPS =100; //used for the timer, will help determine the number of times the game will refresh per second (frames/sec)
+	int check; //will record Marios last left or right movement to decide which side he will face after the key is left
+	int dir; //the initial direction of Mario is set to down
 };
