c468acf8725dbde520c4386f9b3c354bd34e96c8
Devashnee <devashneebhagawandin@hotmail.com>
2016-04-11 22:08:09 +0200

    addions to world

---
+++ SuperMarioSanAndreas/world.cpp
@@ -0,0 +1,15 @@
+#include <allegro5/allegro.h>
+#include <iostream> 
+#include <allegro5/allegro_image.h>
+using namespace std;
+#define length 800
+#define width 600
+ int world(){
+	ALLEGRO_DISPLAY *display;
+	al_set_new_display_flags(ALLEGRO_WINDOWED);
+	display = al_create_display(length, width);
+	al_set_window_position(display, 200, 100);
+	al_rest(5.0);
+	al_destroy_display(display);
+	return 0;
+}
