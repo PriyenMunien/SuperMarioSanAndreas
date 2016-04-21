36f0be1a1c424bde1c2901285f1fb0c177d8392a
PriyenMunien <214529934@stu.ukzn.ac.za>
2016-04-21 08:11:36 +0200

    Splash screens

---
+++ SuperMarioSanAndreas/SplashScreens.cpp
@@ -0,0 +1,8 @@
+#include <allegro5/allegro.h>
+#include <iostream> 
+#include <allegro5/allegro_image.h>
+#include <allegro5/allegro_ttf.h>
+#include <allegro5/allegro_font.h>	
+using namespace std;
+#define length 800
+#define width 600
